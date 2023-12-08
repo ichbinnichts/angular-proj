a131a1e5116acd8d29ef3c0c11cda45f2972f951
Nathan Faria <nathanfaria607@gmail.com>
2023-12-08 14:29:00 -0300

    feat: add toggle form via add and close button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  title: string = 'Task Tracker'
+  title: string = 'Task Tracker';
+  showAddTask: boolean = false;
+  subscription!: Subscription;
 
+  constructor(private uiService: UiService){
+    this.subscription = this.uiService.onToggle().subscribe((value) => this.showAddTask = value);
+  }
 
   toggleAddTask(){
-    console.log('Toggle add task');
+    this.uiService.toggleAddTask();
   }
 }
