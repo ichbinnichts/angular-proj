84f639ed778ac6c6ea879cfc19581090f2d0cc23
Nathan Faria <nathanfaria607@gmail.com>
2023-12-08 10:59:45 -0300

    feat: add fetch data from json server instead of mocked data locally

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -9,10 +9,10 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 })
 export class TaskService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:5000/tasks';
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]>{
-    const tasks =  of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
