475dc07a36cf0815bf8f7afc54e7d196debcea3d
Nathan Faria <nathanfaria607@gmail.com>
2023-12-08 09:29:22 -0300

    feat: define the button component

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css']
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit{
+  @Input() text!: string;
+  @Input() color!: string;
 
+  constructor(){}
+  ngOnInit(): void {}
 }
